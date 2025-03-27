fdac80173dbf0abe028ee66764b0fde30cd7d515
sujansijakhow <sujansijakhow@gmail.com>
2025-03-27 20:44:02 +0545

    Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>Dasboard</h1>
+        <div>
+            <input type="text" placeholder='hlh' />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
